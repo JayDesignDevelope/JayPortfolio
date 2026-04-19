1d0dbcc46109461cec0098c00ea84a86aaa6aaf4
JayDesignDevelope <jay.design.developer@gmail.com>
2026-04-19 05:41:49 +0530

    Stop tracking config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Replace the string below with your actual Google Gemini API Key
-// You can get one for free at: https://aistudio.google.com/app/apikey
-const CONFIG = {
-    GEMINI_API_KEY: "AIzaSyBAu8venRxWBYXOfVNxsLwQzaU8TaRKYvc"
-};
